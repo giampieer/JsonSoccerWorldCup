6c4df14f3d1cf6b0fd7dcd93b19253e7098d053a
Giampieer Mariscal <giampieer24@gmail.com>
2018-05-27 14:57:14 -0500

    prediction

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
 header('Content-Type: application/json');
-$dataArray[]= ['team_a' => "Peru", 'team_b' => "Escocia", 'prediction_a' => "90%", 'prediction_b' => "20%"];
+$dataArray[]= ['team_a' => "Peru", 'team_b' => "Escocia", 'prediction_a' => "71%", 'prediction_b' => "20%"];
 echo  json_encode($dataArray);
 ?>
